0c5a5dfc7632d6a81dd904bb666d4c027e508fa4
fast1sh <dlemeshko04@gmail.com>
2019-04-20 22:21:41 +0300

    Responsive styles for table

--- src/components/item-table/item-table.js
+++ src/components/item-table/item-table.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Table } from "react-bootstrap";
+import { Table, Container, Row, Col } from "react-bootstrap";
 import './item-table.css';
 import { ItemTableHeader, ItemTableBody } from "../item-table-components";
 
@@ -7,14 +7,22 @@ const ItemTable = (props) => {
   const { items, labels, onDeleted, onEdit } = props;
 
   return (
-    <Table>
-      <thead>
-        { ItemTableHeader(labels) }
-      </thead>
-      <tbody>
-        <ItemTableBody items={items} onEdit={onEdit} onDeleted={onDeleted} />
-      </tbody>
-    </Table>
+    <div className="table-wrap">
+      <Container>
+        <Row>
+          <Col>
+            <Table>
+              <thead>
+                {ItemTableHeader(labels)}
+              </thead>
+              <tbody>
+                <ItemTableBody items={items} onEdit={onEdit} onDeleted={onDeleted} />
+              </tbody>
+            </Table>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   )
 };
 
