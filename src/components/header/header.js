69a10609f9dbf064d6bfef58a2b7d1b2afcece25
fast1sh <dlemeshko04@gmail.com>
2019-04-21 20:14:37 +0300

    Change Header links

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -9,25 +9,25 @@ const Header = (props) => {
         <Row>
           <Col>
             <Navbar>
-              <Navbar.Brand href="home" id="navbar-brand">Invoice App</Navbar.Brand>
+              <Navbar.Brand href="/" id="navbar-brand">Invoice App</Navbar.Brand>
               <Navbar.Collapse>
                 <Nav>
                   <Nav.Item>
                     <OverlayTrigger placement="bottom" overlay={<Tooltip id="tooltip-disabled">In progress</Tooltip>}>
                         <div>
-                        <Nav.Link href="invoices" disabled>Invoices</Nav.Link>
+                        <Nav.Link href="#" disabled>Invoices</Nav.Link>
                         </div>
                     </OverlayTrigger>
                   </Nav.Item>
                   <Nav.Item>
                     <OverlayTrigger placement="bottom" overlay={<Tooltip id="tooltip-disabled">In progress</Tooltip>}>
                       <div>
-                        <Nav.Link href="products" disabled>Products</Nav.Link>
+                        <Nav.Link href="#" disabled>Products</Nav.Link>
                       </div>
                     </OverlayTrigger>
                   </Nav.Item>
                   <Nav.Item>
-                    <Nav.Link href="customers">Customers</Nav.Link>
+                    <Nav.Link href="/">Customers</Nav.Link>
                   </Nav.Item>
                 </Nav>
               </Navbar.Collapse>
